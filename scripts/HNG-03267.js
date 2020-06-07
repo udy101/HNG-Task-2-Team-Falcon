eb1fc3089ae3021e607560ef0177aff4a0edc966
INYANG INYANG <inyangamosinyang@gmail.com>
2020-06-07 09:34:08 +0100

    Update HNG-03267.js

--- scripts/HNG-03267.js
+++ scripts/HNG-03267.js
@@ -1,1 +1,1 @@
-console.log("Hello World,this is INYANG AMOS INYANG with HNGi7 ID 03267 using javascript for stage 2 task inyangamosinyang@gmail.com");
+console.log("Hello World,this is INYANG AMOS INYANG with HNGi7 ID 03267 using javascript for stage 2 task.inyangamosinyang@gmail.com")
